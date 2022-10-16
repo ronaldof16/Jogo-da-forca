24859912a5836833314f1bbdec24ec419d572225
Ronaldo Fernando <ronaldof16@hotmail.com>
2022-10-16 01:23:56 -0300

    Conclusão do tabuleiro do jogo

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let palavras = ['ALURA', 'FORCA', 'HTML', 'ORACLE', 'JAVASCRIPT', 'LOGICA', 'PROGRAMA', 'DESAFIO'];
+let palavras = ['FUTEBOL', 'FORCA', 'SORVETE', 'ORACLE', 'JAVASCRIPT', 'LOGICA', 'PROGRAMA', 'DESAFIO'];
 let tabuleiro = document.getElementById('forca').getContext('2d');
 let palavraSecreta = "";
 
 
 function escolherPalavraSecreta() {
-    let palavra = palavras[Math.floor(Math.random() * palavras.length)]
-    palavraSecreta = palavra
-    console.log(palavra)
-    return palavra
+    let palavra = palavras[Math.floor(Math.random() * palavras.length)];
+    palavraSecreta = palavra;
+    console.log(palavra);
+    return palavra;
   }
 
   function iniciaJogo(){
     document.getElementById("div-desaparece").style.display = 'none';
-    escolherPalavraSecreta()
+    escolherPalavraSecreta();
     
-    desenharCanvas()
-    desenharLinhas()
+    desenharCanvas();
+    desenharLinhas();
   }
